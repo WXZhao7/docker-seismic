4ec4b0c3d367459004703b941eadb7b28bb7a4be
WXZhao7 <WXZhao7@gmail.com>
2022-10-31 22:42:28 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -2,24 +2,24 @@
 
 # Date: 2022-10-31 19:10:21
 # Author: WXZhao
-# Description: run this scrip with user in docker group
+# Description: run this scrip with USER in docker group
 
 
-uid=$(id -u)
-gid=$(id -g)
-user=$(id -u -n)
-# keep the original user and group
+UID=$(id -u)
+GID=$(id -g)
+USER=$(id -u -n)
+# keep the original USER and group
 
 cat << EOF > ./Dockerfile
 FROM ubuntu:20.04
 LABEL maintainer="wxzhao7@gmail.com"
 # the packages should download and save in the ./sources/temp
 COPY ./sources /sources
-RUN groupadd -g ${gid} $user && useradd -rm -d /home/${user} -s /bin/bash -g ${gid} -G root -u ${uid} ${user}
 USER root
-RUN bash /sources/install.sh && rm -rf /sources/temp && cp /root/.bashrc /home/${user}/.bashrc
-USER ${user}
-WORKDIR /home/${user}
+RUN groupadd -g $GID $USER && USERadd -rm -d /home/$USER -s /bin/bash -g $GID -G root -u $UID $USER
+RUN bash /sources/install.sh && rm -rf /sources/temp && cp /root/.bashrc /home/$USER/.bashrc
+USER $USER
+WORKDIR /home/$USER
 CMD ["/bin/bash"]
 EOF
 
